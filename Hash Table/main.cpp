2d40feab913420d041dbd48c88f018fa53059733
diogommartins <diogo.martins@unirio.br>
2015-11-15 17:36:38 -0200

    Initial Commit

---
+++ Hash Table/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hash Table
+//
+//  Created by Diogo Magalhães Martins on 11/15/15.
+//  Copyright © 2015 Diogo Magalhães Martins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
